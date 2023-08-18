549f50c8fa59cd94dc396892dfec4cb0ea77dad1
Tyler Zytkowski <87623562+tzytkowski@users.noreply.github.com>
2023-08-18 18:33:21 -0400

    Update Monthly_Savings_Codio.py

--- Monthly_Savings_Codio.py
+++ Monthly_Savings_Codio.py
@@ -1,3 +1,15 @@
 names = ["Bob", "Mary", "Sue", "John", "Mike", "Becky"]
 x = names.pop(3)
-print(x)
+print()
+
+wallet = int()
+dollar = float()
+
+def Spend(): 
+    
+
+
+def Withdraw():
+    print("How much?")
+    
+    wallet = wallet - dollar 
