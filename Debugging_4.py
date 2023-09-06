a940e7631bccc4121e4799861c71243bd3289e13
Tyler Zytkowski <87623562+tzytkowski@users.noreply.github.com>
2023-09-06 08:47:23 -0400

    Update Debugging_4.py

--- Debugging_4.py
+++ Debugging_4.py
@@ -1,25 +1,23 @@
 class FIGHTER:
-    def __init__(self, name, heavyweight, middleweight, lightweight, nationality):
+    def __init__(self, name, weight, nationality, skill):
         self.name = name
-        self.heavyweight = heavyweight
-        self.middleweight = middleweight
-        self.lightweight = lightweight
+        self.weight = weight
         self.nationality = nationality
         self.skill = skill
 
-fighters1 = [FIGHTER('Fedor', True, False, False, "Russian", "Elite"),
-            FIGHTER('Jon Jones', True, True, False, "American", "Elite"),
-            FIGHTER("Francis Ngannou", True, False, False, "African", "High"),
-            FIGHTER("Chuck Liddell", False, True, False, "American", "Very High"),
-            FIGHTER("Anderson Silva", self , False, True, False, "Brazilian", "Elite"),
-            FIGHTER("Vitor Belfort", True, True, False, "Brazilian", "High")]
+fighters1 = [FIGHTER('Fedor', "265", "Russian", "Elite"),
+            FIGHTER('Jon Jones', "205", "American", "Elite"),
+            FIGHTER("Francis Ngannou", "265", "African", "High"),
+            FIGHTER("Chuck Liddell", "205", "American", "Very High"),
+            FIGHTER("Anderson Silva", "185" , "Brazilian", "Elite"),
+            FIGHTER("Vitor Belfort", "185", "Brazilian", "High")]
 
-fighters2 = [FIGHTER('BJ Penn', True, True, True, "Hawaiian", "Elite"),
-            FIGHTER('GSP', False, True, False, "Canadian", "Elite"),
-            FIGHTER('Colby Covington', False, False, True, "American", "Very High"),
-            FIGHTER("Nick Diaz", False, True, True, "American", "Very High"),
-            FIGHTER("Robbie Lawler", False, True, True, "American", "Very High"),
-            FIGHTER("Jorge Masvidal", False, True, "True", "Cuban", "High")]
+fighters2 = [FIGHTER('BJ Penn', "155", "Hawaiian", "Elite"),
+            FIGHTER('GSP', "170", "Canadian", "Elite"),
+            FIGHTER('Colby Covington', "170", "American", "Very High"),
+            FIGHTER("Nick Diaz", "170", "American", "Very High"),
+            FIGHTER("Robbie Lawler", "170", "American", "Very High"),
+            FIGHTER("Jorge Masvidal", "170", "Cuban", "High")]
 
 index = 1;
 # Comparing fighters nationalites
@@ -30,10 +28,10 @@ while index in range(len(fighters1)):
         elif fighters1[index].nationality != fighters2[i].nationality:
             print(fighters1[index].name, "and", fighters2[i].name, "are not the same nationality")
         if fighters1[index].skill == fighters2[i].skill:
-            print("Both", fighters1[index].name, "and", fighters2[i].name, "both have a(n)", fighters2[i].skill, "skillset",)
-            if fighters1[index].heavyweight == True and fighters2[i].heavyweight == True:
+            print("Both", fighters1[index].name, "and", fighters2[i].name, "both have a(n)", fighters2[i].skill, "skillset")
+            if fighters1[index].weight and fighters2[index]:
                 print("and are able to fight each other")
-            else fighters1[index].heavyweight == True and fighters2[i].heavyweight == False:
+            else fighters1[index].weight <= fighters2[index] or fighters2[index]:
                 print(' but cannot fight eachother')
         # End if statements
 
